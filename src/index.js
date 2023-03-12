60e19483952e9dae8a7907b816514a7a3ee09a7e
jk <jkhirakosocmed@gmail.com>
2023-03-12 19:44:53 +0900

    add function for website footer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './style.css';
+import createImage from './ourRoots';
 
-export const content = document.querySelector('#content');
+const content = document.querySelector('#content');
 
-export function createMainHeader() {
+function createMainHeader() {
   const mainHeader = document.createElement('div');
   mainHeader.id = 'mainHeader';
   content.appendChild(mainHeader);
@@ -25,4 +26,36 @@ export function createMainHeader() {
   mainHeader.appendChild(nav);
 }
 
+function createFooter() {
+  const footer = document.createElement('footer');
+  content.appendChild(footer);
+  footer.appendChild(document.createElement('h1')).textContent = 'SWEET CRAVINGS CO.';
+
+  const icons = document.createElement('div');
+  icons.id = 'icons';
+
+  const mailIcon = document.createElement('box-icon');
+  mailIcon.setAttribute('name', 'envelope');
+  icons.appendChild(mailIcon);
+
+  const facebookIcon = document.createElement('box-icon');
+  facebookIcon.setAttribute('name', 'facebook');
+  facebookIcon.setAttribute('type', 'logo');
+  icons.appendChild(facebookIcon);
+
+  const instagramIcon = document.createElement('box-icon');
+  instagramIcon.setAttribute('name', 'instagram');
+  instagramIcon.setAttribute('type', 'logo');
+  icons.appendChild(instagramIcon);
+
+  const pinterestIcon = document.createElement('box-icon');
+  pinterestIcon.setAttribute('name', 'pinterest');
+  pinterestIcon.setAttribute('type', 'logo');
+  icons.appendChild(pinterestIcon);
+
+  footer.appendChild(icons);
+}
+
 createMainHeader();
+createImage();
+createFooter();
