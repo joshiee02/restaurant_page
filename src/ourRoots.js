cd4e477f9cca743885965838225ff75ca00907ee
jk <jkhirakosocmed@gmail.com>
2023-03-12 19:43:41 +0900

    remove import dependency issue

--- src/ourRoots.js
+++ src/ourRoots.js
@@ -1,5 +1,5 @@
-import { content, createMainHeader } from './index';
-
-const img = document.createElement('img');
-img.setAttribute('src', './img/main-photo.jpg');
-content.appendChild('img');
+export default function createImage() {
+  const img = document.createElement('img');
+  img.setAttribute('src', '/src/img/main-photo.jpg');
+  content.appendChild(img);
+}
