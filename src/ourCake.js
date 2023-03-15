4071ffe6543097fb04564bd713ee423a38e5b29d
jk <jkhirakosocmed@gmail.com>
2023-03-15 20:33:54 +0900

    add properties as params on additionalUI func in index.js

---
+++ src/ourCake.js
@@ -0,0 +1,23 @@
+const ourCake = {
+  imgsrc: '/src/img/our-cakes.jpg',
+  h1: 'OUR CAKES',
+
+  firstParagraph: `This is the perfect place for generating visitor interest by going into more detail about yourself,
+    your project, or your mission. You can talk about how your idea started, how long you've been working on it,
+    what it stands for, and why it's important. The more specific you are, the more visitors can engage with what you do
+    . This is also an opportunity to answer any questions they may have about you or your work.`,
+
+  secondParagraph: `Foster's long, scrolling homepage is actually an Index Page, which stacks multiple pages vertically. Under a Foster Index Page,
+    any regular Page with a background image supports parallax scrolling,
+    a 3D effect that creates depth and immersion.
+    Learn more about Foster's Index Page here.`,
+
+  thirdParagraph: `The Foster template supports three separate navigation areas: two in the site header section
+    and one in the footer section. All navigation areas can be styled independently. For example,
+    to give prominence to certain pages in your site, you can include them in the header navigation.
+    Other pages with secondary importance can go in the footer. You can create drop-down menus by
+    placing pages in a folder in the Pages Panel. Learn more about Foster's navigation features here.`,
+
+};
+
+export default ourCake;
